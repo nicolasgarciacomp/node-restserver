0a5c4ed13f832c00bcfd017428c8c05ae9b3c80d
Nicolas <nicolasgarciacomp@gmail.com>
2019-12-15 18:35:14 +0000

    ...

--- server/server.js
+++ server/server.js
@@ -1,47 +1,19 @@
 require('./config/config.js');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
- 
-app.get('/usuario', function(req, res) {
 
-	res.send('get Usuario');
+app.use(require('./routes/usuario.js'));
 
-});
+mongoose.connect('mongodb://localhost:27017/cafe', (err) => {
+	
+	if(err) throw err;
 
-app.post('/usuario', function(req, res) {
-
-	let body = req.body;
-
-	if(body.nombre === undefined) {
-		res.status(400).json({
-			ok: false,
-			mensaje: 'El nombre es necesario'
-		});
-	} else {
-		res.json({
-			persona: body
-		});
-	}
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-	let id = req.params.id;
-
-	res.json({
-		id
-	});
-
-});
-
-app.delete('/usuario', function(req, res) {
-
-	res.send('delete Usuario');
+	console.log('Base de datos ONLINE');
 
 });
  

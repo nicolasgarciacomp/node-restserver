de8733634b9fbec40226ecf571746fb501131d0c
Nicolas <nicolasgarciacomp@gmail.com>
2021-02-15 20:33:25 +0000

    ultomos cambios...

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ require('./config/config.js');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Habilitar public
+app.use(express.static(path.resolve(__dirname + '../public')));
 
 // Configuracion Global de Rutas
 app.use(require('./routes/index.js'));
